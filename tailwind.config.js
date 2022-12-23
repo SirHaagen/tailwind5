db4f7e067d599a91dc50caa009fc10e0e889a26f
Fabian Leon <96808937+SirHaagen@users.noreply.github.com>
2022-12-23 10:43:00 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.{html,js}'
+  ],
+  theme: {
+    extend: {
+      colors:{
+        danger: '#ff5f40',
+        crayola_red: '#ff355e',
+        darkblue: {
+          100: '#000066',
+          200: '#000033'
+        }
+      },
+      borderRadius:{
+        x20: '20px'
+      },
+      width:{
+        vw90: '90vw'
+      }
+    },
+  },
+  plugins: [],
+}
